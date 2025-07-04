c020e76f956f4c92c7cb9044a7ed2da1306e278a
qinxinxin <1248932357@qq.com>
2025-07-05 07:17:42 +0800

    11

--- src/com/java1234/git/Test.java
+++ src/com/java1234/git/Test.java
@@ -2,6 +2,8 @@ package com.java1234.git;
 
 public class Test {
     public static void main(String[] args) {
+
         System.out.println("hello world");
+        System.out.println("hello marry");
     }
 }
